97cf144cc486ce64c64b3ae87885490880fffa07
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2024-02-24 22:13:59 -0600

    Edicion de cotizacion lista

--- SISTEMA/profac-app/app/Http/Livewire/Cotizaciones/ListarCotizaciones.php
+++ SISTEMA/profac-app/app/Http/Livewire/Cotizaciones/ListarCotizaciones.php
@@ -100,7 +100,10 @@ class ListarCotizaciones extends Component
                         <button data-toggle="dropdown" class="btn btn-warning dropdown-toggle" aria-expanded="false">Ver
                             más</button>
                         <ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; top: 33px; left: 0px; will-change: top, left;">
-
+                            
+                            <li>
+                                 <a class="dropdown-item" target="_blank"  href="/cotizacion/edicion/'.$cotizacion->id.'" > <i class="fa-solid fa-file-invoice text-info"></i> Editar </a>
+                            </li>
                             <li>
                                 <a class="dropdown-item" target="_blank"  href="/cotizacion/facturar/'.$cotizacion->id.'" > <i class="fa-solid fa-file-invoice text-info"></i> Facturar </a>
                             </li>
