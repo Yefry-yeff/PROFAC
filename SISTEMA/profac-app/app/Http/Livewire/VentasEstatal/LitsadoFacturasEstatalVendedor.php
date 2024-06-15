0f08363d077458f1b5b5560376c8c2dc6515ec2a
yefry-ortiz-sgsas <55094597+Yefry-yeff@users.noreply.github.com>
2024-06-15 09:12:10 -0600

    Creo que estamos bien

--- SISTEMA/profac-app/app/Http/Livewire/VentasEstatal/LitsadoFacturasEstatalVendedor.php
+++ SISTEMA/profac-app/app/Http/Livewire/VentasEstatal/LitsadoFacturasEstatalVendedor.php
@@ -107,29 +107,26 @@ class LitsadoFacturasEstatalVendedor extends Component
 
             ->addColumn('estado_cobro', function ($listaFacturas) {
 
-
                 $revision = DB::SELECTONE("
-                SELECT IF(COUNT(*), aplicacion_pagos.estado_cerrado, 0) AS 'cerrado'
-                    from aplicacion_pagos
-                    where aplicacion_pagos.estado = 1
-                    and aplicacion_pagos.factura_id =
-                    ".$listaFacturas->id);
+                SELECT IF(COUNT(*), aplicacion_pagos.saldo, -1) AS 'cerrado'
+                from aplicacion_pagos
+                where aplicacion_pagos.estado = 1 and aplicacion_pagos.factura_id =
+                ".$listaFacturas->id);
 
 
-                    if(  $revision->cerrado == 2){
+                if( $revision->cerrado == 0){
 
-                        return
-                        '
+                    return
+                    '
+                    <p class="text-center" ><span class="badge badge-primary p-2" style="font-size:0.75rem">Cerrada</span></p>
+                    ';
 
-                        <p class="text-center" ><span class="badge badge-primary p-2" style="font-size:0.75rem">Cerrada</span></p>
-                        ';
-
-                    }else{
-                        return
-                        '
-                        <p class="text-center"><span class="badge badge-danger p-2" style="font-size:0.75rem">Pendiente</span></p>
-                        ';
-                    }
+                }else{
+                    return
+                    '
+                    <p class="text-center"><span class="badge badge-danger p-2" style="font-size:0.75rem">Pendiente</span></p>
+                    ';
+                }
            })
             ->rawColumns(['opciones','estado_cobro'])
             ->make(true);
