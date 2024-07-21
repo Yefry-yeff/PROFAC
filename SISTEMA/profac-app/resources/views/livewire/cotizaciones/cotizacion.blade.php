3f7db9534589610b1b3ea4232a7e3c3fcc8d4721
yefry-ortiz-sgsas <55094597+Yefry-yeff@users.noreply.github.com>
2024-07-21 09:13:12 -0600

    Revert "k"
    This reverts commit d7347ee74b566aab38654125cf0ddccc05a93847.

--- SISTEMA/profac-app/resources/views/livewire/cotizaciones/cotizacion.blade.php
+++ SISTEMA/profac-app/resources/views/livewire/cotizaciones/cotizacion.blade.php
@@ -49,12 +49,6 @@
         </style>
     @endpush
 
-
-
-    <h1>Scanner de Código de Barras</h1>
-    <div id="interactive" class="viewport"></div>
-    <p id="result"></p>
-
     <div class="row wrapper border-bottom white-bg page-heading">
         <div class="col-lg-8 col-xl-10 col-md-8 col-sm-8">
             <h2>Cotización</h2>
@@ -500,92 +494,6 @@
 
     @push('scripts')
         <script>
-
-            ///////////////////////////////////////////////////
-
-
-//crea elemento
-const video = document.createElement("video");
-
-//nuestro camvas
-const canvasElement = document.getElementById("qr-canvas");
-const canvas = canvasElement.getContext("2d");
-
-//div donde llegara nuestro canvas
-const btnScanQR = document.getElementById("btn-scan-qr");
-
-//lectura desactivada
-let scanning = false;
-
-//funcion para encender la camara
-const encenderCamara = () => {
-  navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function (stream) {
-      scanning = true;
-      btnScanQR.hidden = true;
-      canvasElement.hidden = false;
-      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-      video.srcObject = stream;
-      video.play();
-      tick();
-      scan();
-    });
-};
-
-//funciones para levantar las funiones de encendido de la camara
-function tick() {
-  canvasElement.height = video.videoHeight;
-  canvasElement.width = video.videoWidth;
-  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
-  scanning && requestAnimationFrame(tick);
-}
-
-function scan() {
-  try {
-    qrcode.decode();
-  } catch (e) {
-    setTimeout(scan, 300);
-  }
-}
-
-//apagara la camara
-const cerrarCamara = () => {
-  video.srcObject.getTracks().forEach((track) => {
-    track.stop();
-  });
-  canvasElement.hidden = true;
-  btnScanQR.hidden = false;
-};
-
-const activarSonido = () => {
-  var audio = document.getElementById('audioScaner');
-  audio.play();
-}
-
-//callback cuando termina de leer el codigo QR
-qrcode.callback = (respuesta) => {
-  if (respuesta) {
-    //console.log(respuesta);
-    Swal.fire(respuesta)
-    activarSonido();
-    //encenderCamara();
-    cerrarCamara();
-
-  }
-};
-//evento para mostrar la camara sin el boton
-window.addEventListener('load', (e) => {
-  encenderCamara();
-})
-
-
-
-
-            ///////////////////////////////////////////////////
-
-
             var numeroInputs = 0;
             var arregloIdInputs = [];
             var retencionEstado = false; // true  aplica retencion, false no aplica retencion;
