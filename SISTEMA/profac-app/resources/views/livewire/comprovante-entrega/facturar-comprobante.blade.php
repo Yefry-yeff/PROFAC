a46781081bc3a800d9dad9ca017fe7e1c8347f42
cleeper20 <39780147+cleeper20@users.noreply.github.com>
2024-09-16 18:47:48 -0600

    funcionalidad del 25 porciento

--- SISTEMA/profac-app/resources/views/livewire/comprovante-entrega/facturar-comprobante.blade.php
+++ SISTEMA/profac-app/resources/views/livewire/comprovante-entrega/facturar-comprobante.blade.php
@@ -126,7 +126,7 @@
                                             aplicado %
                                             :<span class="text-danger">*</span></label>
 
-                                        <input class="form-control" type="number" min="0" max="15"
+                                        <input class="form-control" type="number" min="0" max="25"
                                             value="{{ $comprobante->porc_descuento }}" id="porDescuento"
                                             name="porDescuento" onchange="calcularTotalesInicioPagina()"
                                             data-parsley-required>
