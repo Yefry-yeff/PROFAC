81f8118a88646221b0a9d6c9c9c4b613bb9be52e
Jsruiz97 <167387214+Jsruiz97@users.noreply.github.com>
2025-01-25 02:40:34 -0600

    Funciona Reporte Cierre Diario
    Mejoras que faltan
    -Hacerlo por SP
    -hacer responsive el menu de los botones.
    -Validaciones cuando no se coloca la fecha.
    -Mejoras Visuales

--- SISTEMA/profac-app/app/Http/Livewire/Reportes/Cierrediariorep.php
+++ SISTEMA/profac-app/app/Http/Livewire/Reportes/Cierrediariorep.php
@@ -4,13 +4,9 @@ namespace App\Http\Livewire\Reportes;
 
 use Livewire\Component;
 use Illuminate\Database\QueryException;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\DB;
-use Auth;
-use Validator;
-use PDF;
 use Yajra\DataTables\DataTables;
-use Luecano\NumeroALetras\NumeroALetras;
+
 class CierreDiariorep extends Component
 {
     public function render()
@@ -21,7 +17,35 @@ class CierreDiariorep extends Component
     public function consulta($fecha)
     {
         try {
-            $consulta = DB::select("CALL sp_repo_Cirre_Diario(?)", [$fecha]);
+            $consulta = DB::select("
+                SELECT
+                    bc.fechaCierre AS 'FECHA DE CIERRE',
+                    cc.nombre_userCierre AS 'REGISTRADO POR',
+                    cc.estadoDescripcion AS 'ESTADO DE CAJA',
+                    f.id,
+                    f.cai,
+                    f.numero_factura AS 'FACTURA',
+                    cc.cliente AS 'CLIENTE',
+                    cc.vendedor AS 'VENDEDOR',
+                    cc.subtotal AS 'SUBTOTAL FACTURADO',
+                    cc.imp_venta AS 'ISV FACTURADO',
+                    cc.total AS 'TOTAL FACTURADO',
+                    cc.tipoFactura  AS 'CALIDAD DE FACTURA',
+                    cc.tipo AS 'TIPO DE CLIENTE',
+                    CASE
+                        WHEN ac.id_tipo_pago_cobro IS NULL THEN 'N/A'
+                        ELSE tpc.descripcion
+                    END AS 'PAGO POR',
+                    b.nombre AS 'BANCO',
+                    ac.monto_abonado AS 'ABONO',
+                    ac.fecha_pago AS 'FECHA DE PAGO'
+                FROM cierrecaja cc
+                INNER JOIN bitacoracierre bc ON bc.id = cc.bitacoraCierre_id
+                INNER JOIN factura f ON f.cai = cc.factura AND cc.cliente = f.nombre_cliente
+                INNER JOIN abonos_creditos ac ON ac.factura_id = f.id
+                INNER JOIN banco b ON b.id = ac.banco_id
+                LEFT JOIN tipo_pago_cobro tpc ON tpc.id = ac.id_tipo_pago_cobro
+                WHERE bc.fechaCierre = ?", [$fecha]);
 
             return Datatables::of($consulta)
                 ->rawColumns([])
@@ -34,5 +58,4 @@ class CierreDiariorep extends Component
             ], 402);
         }
     }
-
 }
--- SISTEMA/profac-app/resources/views/livewire/reportes/cierrediariorep.blade.php
+++ SISTEMA/profac-app/resources/views/livewire/reportes/cierrediariorep.blade.php
@@ -1,3 +1,74 @@
 <div>
-    <h1>Hola</h1>
+    <style>
+        tfoot input {
+            width: 100%;
+            padding: 3px;
+            box-sizing: border-box;
+        }
+    </style>
+    <h1>Reporte de Cierre Diario</h1>
+
+    <div class="pb-0 wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-content">
+                        <div class="row">
+    <div class="form-group">
+        <label for="fecha_cierre">Fecha de Cierre:</label>
+        <input type="date" id="fecha_cierre" class="form-control" onchange="cargaCierreDiario()">
+    </div>
 </div>
+</div>
+</div>
+</div>
+</div>
+    <p> <b>Nota: </b> Se requiere de selección de una fechas para mostrar la información.</p>
+
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-content">
+                        <div class="table-responsive">
+    <table id="tbl_cierre_diario" class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>FECHA DE CIERRE</th>
+                <th>REGISTRADO POR</th>
+                <th>ESTADO DE CAJA</th>
+                <th>ID FACTURA</th>
+                <th>CAI</th>
+                <th>FACTURA</th>
+                <th>CLIENTE</th>
+                <th>VENDEDOR</th>
+                <th>SUBTOTAL FACTURADO</th>
+                <th>ISV FACTURADO</th>
+                <th>TOTAL FACTURADO</th>
+                <th>CALIDAD DE FACTURA</th>
+                <th>TIPO DE CLIENTE</th>
+                <th>PAGO POR</th>
+                <th>BANCO</th>
+                <th>ABONO</th>
+                <th>FECHA DE PAGO</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+@push('scripts')
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="{{ asset('/js/js_proyecto/reportes/cierrediariorep.js') }}"></script>
+@endpush
