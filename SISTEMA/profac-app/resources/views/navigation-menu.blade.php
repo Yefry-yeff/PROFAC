04ae93a165f659944385fc93995f4ea9e24453a2
Jsruiz97 <167387214+Jsruiz97@users.noreply.github.com>
2025-01-28 18:21:35 -0600

    Cambio a Contabilidad
    Cambio de los reportes de
    Cierre Diario
    Ventas
    Cobro
    al modulo de contabilidad.

--- SISTEMA/profac-app/resources/views/navigation-menu.blade.php
+++ SISTEMA/profac-app/resources/views/navigation-menu.blade.php
@@ -460,6 +460,9 @@
                         <li><a href="/facturaDia" style="color:#ffffff;">Reporte de ventas</a>
                         <li ><a href="/reporte/comision" style="color:#ffffff;">Reporte comisiones</a></li>
                         <li><a href="/reporte/reporteria" style="color:#ffffff;">Reportes Varios</a>
+                        <li ><a href="/reporte/Cierrediariorep" style="color:#ffffff;">Reporte Cierre Diario</a></li>
+                        <li ><a href="/reporte/Librocobrosrep" style="color:#ffffff;">Reporte Libro de Cobros</a></li>
+                        <li ><a href="/reporte/Libroventarep" style="color:#ffffff;">Reporte Libro de Ventas</a></li>
                     </ul>
                 </li>
             @endif
@@ -633,9 +636,6 @@
                     <ul class="nav nav-second-level">
                             <li><a href="/facturaDia" style="color:#ffffff;">Reporte de ventas</a>
                             <li ><a href="/reporte/comision" style="color:#ffffff;">Reporte comisiones</a></li>
-                            <li ><a href="/reporte/Cierrediariorep" style="color:#ffffff;">Reporte Cierre Diario</a></li>
-                            <li ><a href="/reporte/Librocobrosrep" style="color:#ffffff;">Reporte Libro de Cobros</a></li>
-                            <li ><a href="/reporte/Libroventarep" style="color:#ffffff;">Reporte Libro de Ventas</a></li>
                             <li><a href="/reporte/reporteria" style="color:#ffffff;">Reportes Varios</a>
                     </ul>
                 </li>
