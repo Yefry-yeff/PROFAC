323403d8da83eaade3495fd65a7a6c9027d9dabd
yefry-ortiz-sgsas <55094597+Yefry-yeff@users.noreply.github.com>
2025-03-28 08:21:25 -0600

    ll

--- SISTEMA/profac-app/resources/views/livewire/inventario/detalle-producto.blade.php
+++ SISTEMA/profac-app/resources/views/livewire/inventario/detalle-producto.blade.php
@@ -46,7 +46,7 @@
             </ol>
         </div>
 
-        @if (Auth::user()->rol_id == '1' || Auth::user()->rol_id == '5' || Auth::user()->rol_id == '7')
+        @if (Auth::user()->rol_id == '1' || Auth::user()->rol_id == '5' || Auth::user()->rol_id == '7' || Auth::user()->rol_id == '9')
             <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4">
                 <div style="margin-top: 1.5rem" mr-auto>
                     <a href="#" class="btn add-btn btn-warning" data-toggle="modal"
