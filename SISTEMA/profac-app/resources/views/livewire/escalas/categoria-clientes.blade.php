e7a9821d170fdb7dc9b6f085d4ac36547b431697
yefry-ortiz-sgsas <yefryyo@gmail.com>
2025-10-28 18:14:18 -0600

    s

--- SISTEMA/profac-app/resources/views/livewire/escalas/categoria-clientes.blade.php
+++ SISTEMA/profac-app/resources/views/livewire/escalas/categoria-clientes.blade.php
@@ -95,10 +95,67 @@
 
 <div class="d-flex justify-content-between align-items-center mb-3 p-2 bg-light rounded shadow-sm flex-wrap">
     <h3 class="mb-0 text-dark"><b>Categoría de Clientes</b></h3> <h4>Creación de categorías</h4>
-            <button type="submit" class="btn btn-success ml-2" id="btnDescargar">Abrir Ventana de creación</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Abrir Ventana de creación</button>
         </div>
     </form>
 </div>
+{{--  CREACIÓN DE CATEGORIAS DE CLIENTES  --}}
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+{{--  Tabla de categorias de clientes  --}}
+
+<div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-content">
+
+                        <div class="table-responsive">
+                            <table id="tbl_ClientesLista" class="table table-striped table-bordered table-hover">
+                                <thead class="">
+                                    <tr>
+                                        <th>Codigo</th>
+                                        <th>Nombre</th>
+                                        <th>Dirreción</th>
+                                        <th>Teléfono</th>
+                                        <th>Correo</th>
+                                        <th>RTN</th>
+                                        <th>Estado</th>
+                                        <th>Registrado Por:</th>
+                                        <th>Fecha </th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
